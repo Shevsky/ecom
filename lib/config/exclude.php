7a2d07f124e474d08b5d378dae721a86e427e7ea
Konstantin Shevsky <mail@shevsky.com>
2020-01-24 14:12:28 +0500

    Добавлен exclude-файл для сборки.

---
+++ lib/config/exclude.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'__tests',
+	'webpack_js',
+	'composer.json',
+	'composer.lock',
+	'.editorconfig',
+	'phpunit.phar',
+	'lib/vendors/bin',
+	'lib/vendors/lapaygroup/russianpost/tests',
+	'lib/vendors/psr/log/Psr/Log/Test'
+);
